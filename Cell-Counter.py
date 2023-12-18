3c73d17a30d13b89a03a8f77ec0fde90e8ab88d9
seanposner <50656530+seanposner@users.noreply.github.com>
2023-12-18 11:57:37 +0200

    Added manual selection removal

--- Cell-Counter.py
+++ Cell-Counter.py
@@ -3,8 +3,9 @@ import numpy as np
 import cv2
 
 # Define global variables
-global cell_count, marked_image_tolerance
+global cell_count, marked_image_tolerance, manual_cells
 cell_count = 0
+manual_cells = []
 
 def count_and_mark_cells_with_tolerance(image_cv2, target_color_bgr, tolerance):
     # Convert the image to HSV color space for more effective color range matching
@@ -39,15 +40,40 @@ def count_and_mark_cells_with_tolerance(image_cv2, target_color_bgr, tolerance):
 
     return marked_image, cell_count
 
+# Function to update and display the counter
+def update_counter_window():
+    global cell_count
+    counter_image = np.zeros((100, 200, 3), dtype=np.uint8)
+    cv2.putText(counter_image, "Cell Count: {}".format(cell_count), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+    cv2.imshow("Counter", counter_image)
+    
+# Function to check if a point is close to a cell
+def is_close_to_cell(point, cells, threshold=10):
+    for cell in cells:
+        if np.linalg.norm(np.array(cell) - np.array(point)) < threshold:
+            return True, cell
+    return False, None
+
 # Callback function for mouse events
 def click_and_count(event, x, y, flags, param):
-    global cell_count, marked_image_tolerance
+    global cell_count, marked_image_tolerance, manual_cells
     if event == cv2.EVENT_LBUTTONDOWN:
-        # Increase the cell count and draw a circle to mark the manually selected cell
         cell_count += 1
-        cv2.circle(marked_image_tolerance, (x, y), 10, (0, 0, 255), 2)  # Change color or size as needed
+        manual_cells.append((x, y))
+        cv2.circle(marked_image_tolerance, (x, y), 10, (0, 0, 255), 2)
         cv2.putText(marked_image_tolerance, str(cell_count), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.imshow("Image", marked_image_tolerance)
+        update_counter_window()
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        close, cell = is_close_to_cell((x, y), manual_cells)
+        if close:
+            cell_count -= 1
+            manual_cells.remove(cell)
+            # Redraw the image without the removed cell
+            marked_image_tolerance, _ = count_and_mark_cells_with_tolerance(image_cv2.copy(), target_color_bgr, tolerance)
+            for cell in manual_cells:
+                cv2.circle(marked_image_tolerance, cell, 10, (0, 0, 255), 2)
+                cv2.putText(marked_image_tolerance, str(cell_count), cell, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            update_counter_window()
 
 # Load the image
 image_path = '/Users/seanposner/Downloads/CON BSA 2_RGB Trans.tiff'
@@ -77,6 +103,9 @@ marked_image_tolerance_pil = Image.fromarray(cv2.cvtColor(marked_image_tolerance
 # Display the image with OpenCV window
 cv2.namedWindow("Image")
 cv2.setMouseCallback("Image", click_and_count)
+
+# Create and display the initial counter window
+update_counter_window()
 
 # Keep the window open until 'ESC' key is pressed
 while True:
